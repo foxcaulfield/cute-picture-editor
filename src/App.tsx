38457bf76fc5e23f8074720c7a602a9c47e73965
Fox Caulfield <mrcoderprog@gmail.com>
2021-04-24 16:45:14 +0300

    Add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 import MainScreen from "./components/MainScreen/MainScreen";
 import Slider from "./components/Slider/Slider";
@@ -91,6 +91,13 @@ const DEFAULT_OPTIONS: DefaultOptionsType = [
 ];
 
 function App() {
+
+  // #10 define ref for canvas (and set in attributes in JSX)
+  // const resultCanvas = ususeRef
+  const resultCanvas = useRef<HTMLCanvasElement>(null)
+
+  const [imgData, setImgData] = useState({})
+  
   //set default selected edit option
   const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
 
@@ -118,10 +125,89 @@ function App() {
     return { filter: filters.join(" ") };
   }
   console.log(getImageStyle());
+
+  // #04 fall into fileLoad function
+  function loadFile(e:any) //!!!!!!!!!!!!!!!!!!!!!!
+  {
+    // console.log(e)
+
+    // #05 assign our file to variable
+     let file = e.target.files[0];
+
+    //  #06 define FileReader
+     let reader = new FileReader();
+
+     reader.onloadend = function () {
+       //  #08 when reader load - set data via useState hook 
+      setImgData({imgData: reader.result});
+
+// #09 call the loadCanvas function and pass the reader result
+       loadCanvas(reader.result);           
+     }
+
+    //  #07 if there is a file - call readAsData method
+     if (file) {
+       reader.readAsDataURL(file); 
+     }   
+  }
+
+// #12.1 define function to fit canvas to container 
+  function fitToContainer(canvas:any){ //!!!!!!!!!!!!!!!!!!!!
+    canvas.style.width='100%';
+    canvas.style.height='100%';
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+  }
+
+
+  // #11 fall into loadCanvas function
+  function loadCanvas(imgData:any){ //!!!!!!!!!!!!!!!!!!!!!!
+
+// #12 define canvas and 2d context
+    let canvas: any = resultCanvas.current;
+console.log(canvas)
+    const context: any = canvas.getContext("2d"); 
+    console.log(context)
+    // console.log(context)
+    // let _this = this;
+
+    // #12.2 fit canvas to container
+    fitToContainer(canvas);
+    
+    // #13 set image to canvas
+    let image = new Image();
+    image.onload = function() {  
+      context.clearRect(0, 0, canvas.width, canvas.height); 
+      let imgSize = {
+        width: image.width,
+        height: image.height
+      };
+     setImgData({selection: imgSize}); 
+      context.save();
+
+  //large pictures
+// tall picture ( img, left padding, top padding, width, height)
+      {image.height/image.width > canvas.height/canvas.width && 
+      context.drawImage(image, canvas.width/2 - (image.width/(image.height/canvas.height)/2), 0, image.width/(image.height/canvas.height), image.height/(image.height/canvas.height))}
+      
+      // wide picture (img, left padding, top padding, width, height)
+      {image.height/image.width < canvas.height/canvas.width && 
+        context.drawImage(image, 0, canvas.height/2 - (image.height/(image.width/canvas.width)/2), image.width/(image.width/canvas.width), image.height/(image.width/canvas.width))}
+
+//square picture ;)
+
+      // context.drawImage(image, 0, 0, canvas.width, canvas.height);
+      context.restore();
+     setImgData({imgData: canvas.toDataURL('image/jpeg')});
+     setImgData({isSelectDisabled: false});
+    };
+    image.src =  imgData;
+  }
+  
   return (
     <div className="container">
       {/* <MainScreen/> */}
-      <div className="mainImage" style={getImageStyle()} />
+      <div className="mainImage" style={getImageStyle()} >  <canvas className="canvas" ref={resultCanvas} style={getImageStyle()} width="0" height="0"></canvas> </div>
       <div className="sidebar">
         {options.map((option, index) => {
           return (
@@ -141,6 +227,72 @@ function App() {
         value={selectedOption.value}
         handleChange={handleSliderChange}
       />
+      {/* <input type="file" id="fileBrowser" onChange={()=>alert("yes!")} style={{display: 'inline-block'}}/> */}
+      <div>   
+        {/* #01 Define canvas      */}
+        {/* <canvas className="canvas" ref={resultCanvas} style={getImageStyle()} width="500" height="500"></canvas> */}
+        {/* <div  style={this.state.selectorVisibleStyle}>
+          <Selector imgData={this.state.imgData} imgSrc={this.state.imgSrc} img={this.state.canvas} setSelection={(pixels) => {this.setSelection(pixels)}} toggleSelector={(toggle) => {this.toggleSelector(toggle)}}/>
+        </div> */}
+        
+        {/* <div className="editorOptions">*/}
+
+        {/* #02 Define input with file type */}
+          <div className="imageOption">
+
+            {/* #03 load file and call the loadFile function */}
+            <div className="fileInput">
+            <input type="file" id="fileBrowser" onChange={loadFile} style={{display: 'inline-block'}} className="chooseFile"/>
+
+            <br/>
+            <label htmlFor="fileBrowser"> Upload Image <p className="fileName"></p></label> 
+            </div>
+          </div> 
+          {/* <div className="imageOption">
+            <label htmlFor="makeSelection"> Make Selection </label> <br/>
+            <button type="button" id="makeSelection" onClick={(toggle) => {this.toggleSelector(true)}} style={{display: 'inline-block'}} disabled={this.state.isSelectDisabled}>Select </button> 
+          </div> */}
+          
+          {/* <div className="imageOption">
+            <label htmlFor="crop"> Crop </label> <br/>
+            <button type="button" id="crop" onClick={() => {this.cropImg()}} style={{display: 'inline-block'}}  disabled={this.state.isToolsDisabled}> Crop </button>
+          </div> */}
+          
+          {/* <div className="imageOption">
+            <label htmlFor="rotateTxt"> Rotate </label> <br/>
+            <input type="number" id="rotateTxt" ref="rotationTxt" onChange={this.onDegreeChange.bind(this)}  disabled={this.state.isToolsDisabled}/>
+            <button type="button" onClick={(degrees) => { this.rotate(20) }} style={{ display: 'inline-block' }}  disabled={this.state.isToolsDisabled}> Rotate </button>
+          </div> */}
+          
+          
+          {/* <div className="imageOption">
+            <label htmlFor="resize"> Resize </label> <br/>
+            <button type="button" id="resize" onClick={(percentage) => { this.resize(0.5) }} style={{ display: 'inline-block' }}  disabled={this.state.isToolsDisabled}> - </button>
+            <button type="button" onClick={(percentage) => { this.resize(1.5) }} style={{ display: 'inline-block' }}  disabled={this.state.isToolsDisabled}> + </button>
+          </div> */}
+          
+          {/* <div className="imageOption">
+            <label htmlFor="download"> Download Image </label> <br/>
+            <a id="download" ref="downloadBtn" onClick={this.downloadImg.bind(this)} download="face.jpg" href={this.state.imgData}>  <button disabled={this.state.isSelectDisabled}> Download Image </button> </a>
+          </div>
+        </div> */}
+
+        
+        
+        {/* <div className="slider" style={{width: '250px'}}>
+          <h3 htmlFor="rotateSlider"> Rotate Angle </h3>
+          <Slider
+            min={-180}
+            max={180}
+            tooltip={false}
+            value={this.state.rotateSliderValue}
+            onChange={this.handleRotateSlider}
+          />
+          <div className='value'>{this.state.rotateSliderValue}</div>
+        </div> */}
+        
+        
+      </div>
     </div>
   );
 }
